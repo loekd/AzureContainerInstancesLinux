e68f067860b64f4fefdc06eaccaa6c62721caf4b
Loek <admin@admin>
2017-08-14 10:06:36 +0200

    clean projects

---
+++ AzureContainerInstances.JobProcessing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzureContainerInstances.JobProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
