c289ab64e2068bd53bd55d863915948ac6553e11
Loek <admin@admin>
2017-08-14 12:30:46 +0200

    add docker support

---
+++ build-containers.bat
@@ -0,0 +1,9 @@
+docker image build ./azurecontainerinstances.jobgenerator -t loekd/azurecontainerinstances.jobgenerator:latest
+docker image build ./azurecontainerinstances.jobgenerator -t loekd/azurecontainerinstances.jobgenerator:1.0
+
+docker image build ./azurecontainerinstances.jobprocessing -t loekd/azurecontainerinstances.jobprocessing:latest
+docker image build ./azurecontainerinstances.jobprocessing -t loekd/azurecontainerinstances.jobprocessing:1.0
+
+
+docker image build ./azurecontainerinstances.jobprocessing -t loekd/azurecontainerinstances.logging:latest
+docker image build ./azurecontainerinstances.jobprocessing -t loekd/azurecontainerinstances.logging:1.0
---
+++ push-containers.bat
@@ -0,0 +1,8 @@
+docker push loekd/azurecontainerinstances.jobgenerator:latest
+docker push loekd/azurecontainerinstances.jobgenerator:1.0
+
+docker push loekd/azurecontainerinstances.jobprocessing:latest
+docker push loekd/azurecontainerinstances.jobprocessing:1.0
+
+docker push loekd/azurecontainerinstances.logging:latest
+docker push loekd/azurecontainerinstances.logging:1.0
