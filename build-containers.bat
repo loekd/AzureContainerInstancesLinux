92966e0877b9f5af570ac53ff685426c5b512872
Loek <admin@admin>
2017-08-16 09:47:04 +0200

    fixed templates to override env var for logging service location, when deploying to aci

--- build-containers.bat
+++ build-containers.bat
@@ -1,9 +1,7 @@
-docker image build ./azurecontainerinstances.jobgenerator -t loekd/azurecontainerinstances.jobgenerator:latest
-docker image build ./azurecontainerinstances.jobgenerator -t loekd/azurecontainerinstances.jobgenerator:1.0
-
-docker image build ./azurecontainerinstances.jobprocessing -t loekd/azurecontainerinstances.jobprocessing:latest
-docker image build ./azurecontainerinstances.jobprocessing -t loekd/azurecontainerinstances.jobprocessing:1.0
+docker image build ./azurecontainerinstances.jobgenerator -t loekd/azurecontainerinstances.jobgenerator:latest -t loekd/azurecontainerinstances.jobgenerator:1.0
 
 
-docker image build ./azurecontainerinstances.jobprocessing -t loekd/azurecontainerinstances.logging:latest
-docker image build ./azurecontainerinstances.jobprocessing -t loekd/azurecontainerinstances.logging:1.0
+docker image build ./azurecontainerinstances.jobprocessing -t loekd/azurecontainerinstances.jobprocessing:latest -t loekd/azurecontainerinstances.jobprocessing:3.0
+
+
+docker image build ./azurecontainerinstances.logging -t loekd/azurecontainerinstances.logging:latest -t loekd/azurecontainerinstances.logging:1.0
--- push-containers.bat
+++ push-containers.bat
@@ -2,7 +2,7 @@ docker push loekd/azurecontainerinstances.jobgenerator:latest
 docker push loekd/azurecontainerinstances.jobgenerator:1.0
 
 docker push loekd/azurecontainerinstances.jobprocessing:latest
-docker push loekd/azurecontainerinstances.jobprocessing:1.0
+docker push loekd/azurecontainerinstances.jobprocessing:3.0
 
 docker push loekd/azurecontainerinstances.logging:latest
 docker push loekd/azurecontainerinstances.logging:1.0
